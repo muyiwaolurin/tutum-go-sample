34f81f4e8f118fec1fcf36b7f2112f257c3accef
Anthony Olurin <anthony.olurin@metricaid.com>
2015-12-13 03:01:49 -0500

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM golang
  
 ADD . /gocode/src/github.com/muyiwaolurin/tutum-go-sample
+RUN go get gopkg.in/mgo.v2
 RUN go install /github.com/muyiwaolurin/tutum-go-sample
-ENTRYPOINT /go/bin/basic_web_server
+
+ENV NAME world
+
+CMD /go/bin/tutum-go-sample
  
-EXPOSE 8080
+EXPOSE 8282
+
+
--- main.go
+++ main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"runtime"
+	"os"
+	"time"
+
+	"gopkg.in/mgo.v2"
 )
 
+func mongoConnect() (s string) {
+
+	session, err := mgo.Dial("mongo")
+	if err == nil {
+		defer session.Close()
+		s = "connected"
+	} else {
+		s = "not available"
+	}
+	return s
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+
+	hostname, _ := os.Hostname()
+	mongostatus := mongoConnect()
+	fmt.Fprintf(w, "<h1>hello, %s</h1>\n<b>Hostname: </b>%s<br><b>MongoDB Status: </b>%s", os.Getenv("NAME"), hostname, mongostatus)
+	fmt.Println(hostname, "handled HTTP REQUEST at", time.Now(), "\nMongoDB Status:", mongostatus)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8282", nil)
+	fmt.Println("Everything is working !\n Listening on port 80 for requests...")
+	http.ListenAndServe(":80", nil)
 }
